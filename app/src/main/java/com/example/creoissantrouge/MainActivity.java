de04838363050a6af714672a0e6c183379e401d6
SiwarAbid <abidsiwar371@gmail.com>
2023-12-21 17:01:05 +0100

    Pages connexion les interfaces et les liaison entre eux

--- app/src/main/java/com/example/creoissantrouge/MainActivity.java
+++ app/src/main/java/com/example/creoissantrouge/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.creoissantrouge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ButtonCnx;
+
+            ButtonCnx = findViewById(R.id.button1);
+            ButtonCnx.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Sign_Up.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
-}
