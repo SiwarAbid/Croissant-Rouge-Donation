76edd95ad984b369b2092e7b0784d01f8f3d4af3
SiwarAbid <abidsiwar371@gmail.com>
2024-01-09 21:00:01 +0100

    interfaces

--- app/src/main/java/com/example/creoissantrouge/MainActivity.java
+++ app/src/main/java/com/example/creoissantrouge/MainActivity.java
@@ -3,24 +3,60 @@ package com.example.creoissantrouge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText user_name, pwd;
+    Button btn_cnx,ButtonSignUp;
+    SQLiteDatabase bd;
+    Cursor c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button ButtonCnx;
 
-            ButtonCnx = findViewById(R.id.button1);
-            ButtonCnx.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, Sign_Up.class);
-                    startActivity(intent);
-                }
-            });
+        user_name = findViewById(R.id.user_name);
+        pwd = findViewById(R.id.pwd);
+        btn_cnx = findViewById(R.id.button);
+        ButtonSignUp = findViewById(R.id.button1);
+        bd = openOrCreateDatabase("CR", MODE_PRIVATE, null);
+        bd.execSQL("CREATE TABLE IF NOT EXISTS User(id integer primary key autoincrement, username VARCHAR, name VARCHAR, email_phone VARCHAR, pwd VARCHAR);");
+        bd.close();
+        btn_cnx.setOnClickListener(v -> {
+            Intent intent;
+            if(exist(v))
+                intent = new Intent(MainActivity.this, Accueil.class);
+            else
+                intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+
+        });
+        ButtonSignUp.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Sign_Up.class);
+            startActivity(intent);
+        });
         }
+
+        public boolean exist (View v){
+            c = bd.rawQuery("SELECT username, pwd FROM User",null);
+
+            if(c.getString(0).equals(user_name.getText().toString())){
+                if(c.getString(1).equals(pwd.getText().toString())){
+                    Toast.makeText(this, "SUCCEED", Toast.LENGTH_SHORT).show();
+                    return true;
+                }else
+                    Toast.makeText(this, "ERROR! PASSWORD INCORRECT", Toast.LENGTH_SHORT).show();
+
+            }else
+                Toast.makeText(this, "ERROR! USERNAME INCORRECT", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
     }
--- app/src/main/java/com/example/creoissantrouge/Sign_Up.java
+++ app/src/main/java/com/example/creoissantrouge/Sign_Up.java
@@ -1,26 +1,75 @@
 package com.example.creoissantrouge;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Sign_Up extends AppCompatActivity {
+    EditText email, user_name, name, pwd, conf_pwd;
+    Button btn_cnx, ButtonSingIn;
+    SQLiteDatabase bd;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-    setContentView(R.layout.sign_up);
-    Button ButtonCnx;
+        setContentView(R.layout.sign_up);
 
-    ButtonCnx = findViewById(R.id.button1);
-            ButtonCnx.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
+        email = findViewById(R.id.email);
+        user_name = findViewById(R.id.username);
+        name = findViewById(R.id.name);
+        pwd = findViewById(R.id.pwd);
+        conf_pwd = findViewById(R.id.conf_pwd);
+        bd = openOrCreateDatabase("CR", MODE_PRIVATE, null);
+        bd.execSQL("CREATE TABLE IF NOT EXISTS User(id integer primary key autoincrement, username VARCHAR, name VARCHAR, email_phone VARCHAR, pwd VARCHAR);");
+        bd.close();
+        ButtonSingIn = findViewById(R.id.button1);
+
+        btn_cnx = findViewById(R.id.button);
+        btn_cnx.setOnClickListener(v -> {
+            Intent intent;
+            if(create_user(v)){
+                intent = new Intent(Sign_Up.this, Accueil.class);
+                startActivity(intent);
+            }
+
+        });
+        ButtonSingIn.setOnClickListener(v -> {
             Intent intent = new Intent(Sign_Up.this, MainActivity.class);
             startActivity(intent);
+        });
+    }
+
+    public boolean create_user(View v) {
+
+        if (email.getText().toString().trim().length() == 0 || user_name.getText().toString().trim().length() == 0 || name.getText().toString().trim().length() == 0 || pwd.getText().toString().trim().length() == 0 || conf_pwd.getText().toString().trim().length() == 0) {
+            Toast.makeText(this, "ALL REQUIRED!", Toast.LENGTH_SHORT).show();
         }
-    });
-}
+        else {
+            if ((pwd.getText().toString()).equals(conf_pwd.getText().toString())) {
+                bd.execSQL("INSERT INTO User (username, name, email_phone, pwd) VALUES('" + user_name.getText() + "','" + name.getText() + "','" + email.getText() + "','" + pwd.getText() + "');");
+                Toast msg = Toast.makeText(this, "SUCCEED", Toast.LENGTH_LONG);
+                msg.show();
+                vider_Text();
+                bd.close();
+                return true;
+            } else
+                Toast.makeText(this, "PASSWORD NOT COMPATIBLE", Toast.LENGTH_SHORT).show();
+
+        }
+        return false;
+    }
+    public void vider_Text()
+    {
+        user_name.setText("");
+        name.setText("");
+        email.setText("");
+        pwd.setText("");
+        conf_pwd.setText("");
+    }
 }
